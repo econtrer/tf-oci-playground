75edc5d8ec3abc8a1ce7d8c441317329afbd2080
econtrer <eduardo.contrerasd@gmail.com>
2026-02-02 20:54:50 -0300

    Remove instance

--- instance.tf
+++ instance.tf
@@ -14,34 +14,34 @@ data "oci_identity_availability_domain" "ad" {
   ad_number      = 1
 }
 
-resource "oci_core_instance" "free_instance0" {
-  availability_domain = data.oci_identity_availability_domain.ad.name
-  compartment_id      = oci_identity_compartment.tf-compartment.id
-  display_name        = "freeInstance0"
-  shape               = var.instance_shape
+# resource "oci_core_instance" "free_instance0" {
+#   availability_domain = data.oci_identity_availability_domain.ad.name
+#   compartment_id      = oci_identity_compartment.tf-compartment.id
+#   display_name        = "freeInstance0"
+#   shape               = var.instance_shape
 
-  shape_config {
-    ocpus = var.instance_ocpus
-    memory_in_gbs = var.instance_shape_config_memory_in_gbs
-  }
+#   shape_config {
+#     ocpus = var.instance_ocpus
+#     memory_in_gbs = var.instance_shape_config_memory_in_gbs
+#   }
 
-  create_vnic_details {
-    subnet_id        = oci_core_subnet.test_subnet.id
-    display_name     = "primaryvnic"
-    assign_public_ip = true
-    hostname_label   = "freeinstance0"
-  }
+#   create_vnic_details {
+#     subnet_id        = oci_core_subnet.test_subnet.id
+#     display_name     = "primaryvnic"
+#     assign_public_ip = true
+#     hostname_label   = "freeinstance0"
+#   }
 
-  source_details {
-    source_type = "image"
-    source_id   = lookup(data.oci_core_images.test_images.images[0], "id")
-    boot_volume_size_in_gbs = 150
-  }
+#   source_details {
+#     source_type = "image"
+#     source_id   = lookup(data.oci_core_images.test_images.images[0], "id")
+#     boot_volume_size_in_gbs = 150
+#   }
 
-  metadata = {
-    ssh_authorized_keys = var.ssh_public_key
-  }
-}
+#   metadata = {
+#     ssh_authorized_keys = var.ssh_public_key
+#   }
+# }
 
 # resource "tls_private_key" "compute_ssh_key" {
 #   algorithm = "RSA"
