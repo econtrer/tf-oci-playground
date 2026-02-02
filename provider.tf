44432769bb3e9933ac01c58430d3fb179129abc1
econtrer <eduardo.contrerasd@gmail.com>
2026-02-02 17:42:00 -0300

    Testing oci provider without fingerprint

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "oci" {
   region           = var.region
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
+  # fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
 }
