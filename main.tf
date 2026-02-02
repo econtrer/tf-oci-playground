27aec1cde2527bb154f5cca83cedd8a324628eed
econtrer <eduardo.contrerasd@gmail.com>
2026-02-02 17:31:11 -0300

    OCI terraform tutorial

--- main.tf
+++ main.tf
@@ -21,11 +21,3 @@ variable "compartment_ocid" {
 
 variable "region" {
 }
-
-provider "oci" {
-  region           = var.region
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-}
