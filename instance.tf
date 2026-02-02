1f045dd3eed86a932519647f79cf7a3129891de5
econtrer <eduardo.contrerasd@gmail.com>
2026-02-02 20:22:15 -0300

    Compute instance with pre existing ssh key access

---
+++ instance.tf
@@ -0,0 +1,48 @@
+data "oci_core_images" "test_images" {
+  #Required
+  compartment_id = oci_identity_compartment.tf-compartment.id
+
+  #Optional
+  # display_name = var.image_display_name
+  operating_system = var.image_operating_system
+  operating_system_version = var.image_operating_system_version
+  shape = var.instance_shape
+}
+
+resource "oci_core_instance" "free_instance0" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = oci_identity_compartment.tf-compartment.id
+  display_name        = "freeInstance0"
+  shape               = var.instance_shape
+
+  shape_config {
+    ocpus = var.instance_ocpus
+    memory_in_gbs = var.instance_shape_config_memory_in_gbs
+  }
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.test_subnet.id
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "freeinstance0"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = lookup(data.oci_core_images.test_images.images[0], "id")
+  }
+
+  metadata = {
+    ssh_authorized_keys = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.public_key_openssh
+  }
+}
+
+# resource "tls_private_key" "compute_ssh_key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 2048
+# }
+
+# output "generated_private_key_pem" {
+#   value     = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.private_key_pem
+#   sensitive = true
+# }
--- main.tf
+++ main.tf
@@ -25,3 +25,27 @@ variable "region" {
 variable "compartment_name" {
   default = "hytale"
 }
+
+variable "instance_shape" {
+  default = "VM.Standard.A1.Flex" # Always free instance shape
+}
+
+variable "instance_ocpus" {
+  default = 4 # Max OCPUs for always free instance
+}
+
+variable "instance_shape_config_memory_in_gbs" {
+  default = 24 # Max memory for always free instance
+}
+
+variable "ssh_public_key" {
+}
+
+# Instance image details
+variable "image_operating_system" {
+  default = "Oracle Linux"
+}
+
+variable "image_operating_system_version" {
+  default = "9"
+}
